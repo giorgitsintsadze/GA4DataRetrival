1de522a36c38b0dcfa18ba2d12fdadcb8ad43024
Giorgi Tsintsadze <giorgi.tsintsadze@scandiweb.com>
2023-06-20 18:52:50 +0400

    added loader

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const reportForm = document.getElementById('reportForm');
     const reportContainer = document.getElementById('reportContainer');
+    const loader = document.getElementById('loader');
 
     reportForm.addEventListener('submit', async (event) => {
         event.preventDefault();
@@ -8,13 +9,21 @@ document.addEventListener('DOMContentLoaded', () => {
         const dateRange = dateRangeSelect.value;
 
         try {
+            // Show the loader
+            loader.style.display = 'block';
+
             const response = await fetch(`/report?dateRange=${dateRange}`);
             const data = await response.json();
             reportContainer.innerText = data.reportData;
+
+            // Hide the loader
+            loader.style.display = 'none';
         } catch (error) {
             console.error(error);
             reportContainer.innerText = 'Failed to generate the report.';
+
+            // Hide the loader
+            loader.style.display = 'none';
         }
     });
 });
-
